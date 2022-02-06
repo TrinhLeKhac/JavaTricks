f875e972c75b33b677e4c2144c6009153f22c28a
Trinh Le Khac <emperorfire52@gmail.com>
2022-02-06 15:37:34 +0700

    add enum

---
+++ src/Chapter4/StaticMemberClass.java
@@ -0,0 +1,11 @@
+package Chapter4;
+
+/**
+ * Declare a member class that does not require access to an enclosing instance,
+ * put the static modifier in its declaration, if not, instance has a hidden reference to its enclosing instance
+ * Store this reference take time and space
+ * */
+
+public class StaticMemberClass {
+
+}
